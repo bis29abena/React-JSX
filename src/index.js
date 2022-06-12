5e66bdb8e1903ac928b05ed5bb3d52cfd0e82c58
Bismark Osei <bismarkoseiabena@gmail.com>
2022-06-12 20:03:57 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello JSX</h1>
+    <ul>
+      <li>I am happyy</li>
+      <li>To learn this amazing program</li>
+      <li>hello</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
